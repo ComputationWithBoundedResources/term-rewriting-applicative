ef8fe1f6a53cec00a3f9b909d947a16167376f37
Martin Avanzini <martin.avanzini@uibk.ac.at>
2015-10-30 16:19:00 +0100

    added applicative arity

--- src/Data/Rewriting/Applicative/Rules.hs
+++ src/Data/Rewriting/Applicative/Rules.hs
@@ -5,11 +5,14 @@ module Data.Rewriting.Applicative.Rules (
   , funs
   , funsDL
   , amap
+  , applicativeArity
   ) where
 
 import Prelude hiding (map)
 import qualified Prelude as P
+import qualified Data.Map as Map
 import Data.Rewriting.Rules hiding (funs,funsDL)
+import qualified Data.Rewriting.Applicative.Term as T
 import qualified Data.Rewriting.Applicative.Rule as R
 
 
@@ -21,3 +24,17 @@ funsDL rs fs = foldr R.funsDL fs rs
 
 amap :: (f -> f') -> (v -> v') -> [R.ARule f v] -> [R.ARule f' v']
 amap f v = P.map (R.amap f v)
+
+applicativeArity :: Ord f => [R.ARule f v] -> f -> Int
+applicativeArity rs = \ f -> Map.findWithDefault 0 f m
+  where
+    m = Map.fromListWith max (aa [] terms)
+   
+    aa l [] = l
+    aa l ((T.aterm -> T.TVar _ ):ts) = aa l ts    
+    aa l ((T.aterm -> (T.TFun _ ts')):ts) = aa l (ts'++ts)
+    aa l ((T.aform -> Just (T.atermM -> Just (T.TFun f thd),tas)):ts) = 
+      aa ((f,length tas) : l) (thd ++ tas ++ ts)
+    aa l ((T.aform -> Just (_,tas)):ts) = aa l (tas ++ ts)
+    aa l (_:ts) = aa l ts
+    terms = concatMap (\ (R.Rule l r) -> [l,r]) rs
